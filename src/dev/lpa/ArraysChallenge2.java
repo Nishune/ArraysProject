1483683fbc9b8d440968b87dc56bdaa2554a0345
Rikard <rikardengstrom89@gmail.com>
2025-07-01 13:58:08 +0200

    done

--- src/dev/lpa/ArraysChallenge2.java
+++ src/dev/lpa/ArraysChallenge2.java
@@ -6,16 +6,15 @@ public class ArraysChallenge2 {
 
     public static void main(String[] args) {
         int[] userInput = getIntegers(5);
-        printArray(userInput);
+        int[] sortedArray = sortIntegers(userInput);
+        printArray(sortedArray);
 
-        for (int i = 0; i < userInput.length; i++) {
-            System.out.println("Element #" + i + " : " + userInput[i]);
-        }
     }
 
 
     public static int[] getIntegers(int size) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter numbers: ");
         int[] array = new int[size];
 
         for (int i = 0; i < size; i++) {
@@ -26,7 +25,29 @@ public class ArraysChallenge2 {
 
     public static void printArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            System.out.print("Element " + array[i] + " ");
+            System.out.println("Element " + i + " contents " + array[i]);
         }
+    }
+
+    public static int[] sortIntegers(int[] array) {
+
+        int[] sortedArray = array.clone();
+
+        boolean flag = true;
+        int temp;
+
+        while (flag) {
+            flag = false;
+
+            for (int i = 0; i < sortedArray.length - 1; i++) {
+                if (sortedArray[i] < sortedArray[i + 1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
     }
 }
