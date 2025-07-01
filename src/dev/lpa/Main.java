bc828c13b2f1184901ccb7dfb5c831fd2d2473b9
Rikard <rikardengstrom89@gmail.com>
2025-07-01 09:47:54 +0200

    challenge

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -63,8 +63,34 @@ public class Main {
         System.out.println(Arrays.toString(thirdArray));
 
         int[] fourthArray = Arrays.copyOf(thirdArray, thirdArray.length);
-
         System.out.println(Arrays.toString(fourthArray));
+
+        Arrays.sort(fourthArray);
+        System.out.println(Arrays.toString(fourthArray));
+        System.out.println(Arrays.toString(thirdArray));
+
+        int[] smallerArray = Arrays.copyOf(thirdArray, 5);
+        System.out.println(Arrays.toString(smallerArray));
+
+        int[] largerArray = Arrays.copyOf(thirdArray, 15);
+        System.out.println(Arrays.toString(largerArray));
+
+        String[] sArray = {"Able", "Jane", "Mark", "Ralph", "David"};
+        Arrays.sort(sArray); //Must sort array before using binarySearch.
+        System.out.println(Arrays.toString(sArray));
+        if (Arrays.binarySearch(sArray, "Mark") >= 0) {
+            System.out.println("Found Mark in the list.");
+        }
+
+        int[] s1 = {1,2,3,4,5};
+        int[] s2 = {1,2,3,4,5};
+
+        if (Arrays.equals(s1, s2)) {
+            System.out.println("Arrays are equal!");
+        } else {
+            System.out.println("Arrays are not equal!");
+        }
+
 
 
 
